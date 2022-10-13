e3949e656b776a32785b89544fd30a4c4a5e0f87
Piergiuseppe Mallozzi <mallozzi@berkeley.edu>
2022-10-13 14:45:17 -0700

    cleanup

--- src/sym_cps/examples/build_uav.py
+++ src/sym_cps/examples/build_uav.py
@@ -2,32 +2,11 @@ import json
 
 from sym_cps.grammar.topology import AbstractTopology
 from sym_cps.representation.design.concrete import DConcrete
-from sym_cps.representation.design.tools import generate_designs_info_files
 from sym_cps.shared.library import designs
 from sym_cps.shared.paths import ExportType
 from sym_cps.tools.io import save_to_file
 
-"""Generate topology.json from existing designs"""
-
-
-def generate_topology(design_name: str):
-    generate_designs_info_files(designs, design_name)
-
-
 """Generate design from topology.json"""
-
-def analysis():
-    quad = designs["TestQuad"][0]
-    axe = designs["NewAxe"][0]
-
-    print("TestQuad DP")
-    for dp in quad.design_parameters.values():
-        print(dp)
-
-    print("NewAxe DP")
-    for dp in axe.design_parameters.values():
-        print(dp)
-
 def modified_design():
     test_quad_original = designs["TestQuad"][0]
 
@@ -42,7 +21,7 @@ def modified_design():
     f = open(topology_json_path)
     topology_json = json.load(f)
     topology_json["NAME"] = f"{topology_json['NAME']}MOD"
-    new_file_path = save_to_file(
+    save_to_file(
         str(json.dumps(topology_json)),
         file_name=f"topology_summary_mod.json",
         absolute_folder_path=topology_json_path.parent,
@@ -51,7 +30,7 @@ def modified_design():
     # Loading AbstractTopology form file
     abstract_topology = AbstractTopology.from_json(topology_json_path)
     abstract_topology.name += "Modified"
-    new_file_path = save_to_file(
+    save_to_file(
         abstract_topology.to_json(),
         file_name=f"topology_summary_modified.json",
         absolute_folder_path=topology_json_path.parent,
@@ -81,8 +60,4 @@ def modified_design():
 
 
 if __name__ == "__main__":
-    # generate_topology("TestQuad")
-    # topo_file = data_folder / "custom_designs" / "modified_test_quad.json"
-    # generate_design(topo_file)
     modified_design()
-    # analysis()
