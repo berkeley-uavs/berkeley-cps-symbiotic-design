022c2bf8fe5a7dd9a0ff1633959b8203dfddecf4
Piergiuseppe Mallozzi <mallozzi@berkeley.edu>
2022-10-21 16:37:28 -0700

    updated abstraction tests

--- tests/test_abastract_topology.py
+++ tests/test_abastract_topology.py
@@ -1,3 +1,5 @@
+import json
+
 from sym_cps.grammar.topology import AbstractTopology
 from sym_cps.representation.design.concrete import DConcrete
 from sym_cps.shared.designs import designs
@@ -10,15 +12,24 @@ def asset_topology(topology_level: ExportType):
     test_quad_original = designs["TestQuad"][0]
 
     # Exporting AbstractTopology to file
-    topology_json_path = test_quad_original.export(topology_level)
+    topology_json_path_1 = test_quad_original.export(topology_level)
 
     # Loading AbstractTopology from file
-    abstract_topology = AbstractTopology.from_json(topology_json_path)
+    abstract_topology = AbstractTopology.from_json(topology_json_path_1)
 
     # Creating DConcrete Object
     test_quad_loaded = DConcrete.from_abstract_topology(abstract_topology)
 
     assert test_quad_original == test_quad_loaded
+
+    # Exporting AbstractTopology to file
+    topology_json_path_2 = test_quad_loaded.export(topology_level)
+
+    # Comparing jsons
+    json_1 = json.load(open(topology_json_path_1))
+    json_2 = json.load(open(topology_json_path_2))
+
+    assert sorted(json_1.items()) == sorted(json_2.items())
 
 
 levels = [ExportType.TOPOLOGY_1, ExportType.TOPOLOGY_2, ExportType.TOPOLOGY_3]
@@ -26,7 +37,6 @@ levels = [ExportType.TOPOLOGY_1, ExportType.TOPOLOGY_2, ExportType.TOPOLOGY_3]
 
 def test_topology_abstraction_1():
     asset_topology(ExportType.TOPOLOGY_1)
-
 
 def test_topology_abstraction_2():
     asset_topology(ExportType.TOPOLOGY_2)
