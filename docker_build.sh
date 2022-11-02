3b18d625b636929ea2f44d302803d9b998abdaf9
pier <piergiuseppe.mallozzi@gmail.com>
2022-11-02 12:59:27 -0700

    updated docker

--- docker_build.sh
+++ docker_build.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-echo "Building docker image..."
 
 base_image=pmallozzi/devenvs:base-310
 image_name=${base_image}-symcps
 
-docker pull ${base_image}
 
-docker buildx build --push --platform linux/amd64 -f ./Dockerfile -t ${image_name} . --no-cache
-#docker buildx build --push --platform linux/arm64 -f ./Dockerfile -t ${image_name} . --no-cache
+docker pull ${base_image}
+echo "Building docker image..."
+case "${1}" in
+    arm )
+      echo "Building docker image for arm64 architecture"
+      docker buildx build --push --platform linux/arm64 -f ./Dockerfile -t ${image_name} . --no-cache
+      ;;
+    amd )
+      echo "Building docker image for amd64 architecture"
+      docker buildx build --push --platform linux/amd64 -f ./Dockerfile -t ${image_name} . --no-cache
+      ;;
+    * )
+      echo "Building docker image for amd64 and arm64 architecture"
+      docker buildx build --push --platform linux/amd64,linux/arm64 -f ./Dockerfile -t ${image_name} . --no-cache
+      ;;
+
+  esac
