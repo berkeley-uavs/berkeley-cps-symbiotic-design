571c134293b32df26366e3aee925dabb54b64258
pier <piergiuseppe.mallozzi@gmail.com>
2022-11-03 20:29:32 -0700

    small updated

--- src/sym_cps/cli.py
+++ src/sym_cps/cli.py
@@ -2,6 +2,8 @@
 import argparse
 from pathlib import Path
 from typing import List, Optional
+
+from sym_cps.examples.library import export_library
 from sym_cps.representation.design.concrete import DConcrete
 from sym_cps.shared.paths import data_folder
 from sym_cps.tools.update_library import export_all_designs, update_dat_files_and_export
@@ -25,6 +27,7 @@ def _parse_design(args: Optional[List[str]] = None) -> DConcrete:
 
 def update_all() -> int:
     update_dat_files_and_export()
+    export_library()
     return 0
 
 
