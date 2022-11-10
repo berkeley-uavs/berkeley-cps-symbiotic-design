ebdfd32cd61f5cf3519348c2b9031d53ed702f8c
pier <piergiuseppe.mallozzi@gmail.com>
2022-11-09 22:04:47 -0800

    updates, works

--- src/sym_cps/representation/design/concrete/structures.py
+++ src/sym_cps/representation/design/concrete/structures.py
@@ -1,11 +1,15 @@
 from __future__ import annotations
 
+import argparse
+import json
 import os
-from itertools import product
+import random
+from itertools import product, combinations
 
 from sym_cps.representation.design.concrete import DConcrete
 from sym_cps.representation.design.concrete.tools import find_isomorphisms, get_subgraph, is_isomorphism_present
 from sym_cps.shared.designs import designs
+from sym_cps.shared.paths import output_folder
 from sym_cps.tools.graphs import graph_to_dict, graph_to_pdf
 from sym_cps.tools.my_io import save_to_file
 
@@ -28,8 +32,11 @@ def find_isos(designs_to_decompose: list[DConcrete], key_nodes: list[str] | None
                         dec_designs[size].append(dec)
                 else:
                     dec_designs[size] = [dec]
+        if len(dec_designs.keys()) > 0:
+            subgraphs.append(dec_designs)
 
-        subgraphs.append(dec_designs)
+    if len(subgraphs) <= 1:
+        return [], []
 
     n_nodes_min = 100
     n_nodes_max = 1
@@ -71,13 +78,12 @@ def find_isos(designs_to_decompose: list[DConcrete], key_nodes: list[str] | None
             for j, graph in enumerate(groups):
                 graph_to_pdf(graph, f"{n_nodes}_{i}_{j}_graph", export_folder)
 
-    print(f"Found {len(local_iso)} local isomorphisms and {global_iso} global ones")
+    print(f"Found {len(local_iso)} local isomorphisms and {len(global_iso)} global ones")
     return local_iso, global_iso
 
 
 def explore_structures(designs: list[DConcrete], key_nodes: list[str]):
-
-    local_iso, global_iso = find_isos(dconcretes, key_nodes)
+    local_iso, global_iso = find_isos(designs, key_nodes)
 
     key_nodes_str = "-".join(key_nodes)
     export_folder = f"analysis/isomorphisms/{key_nodes_str}/structures"
@@ -94,12 +100,52 @@ def explore_structures(designs: list[DConcrete], key_nodes: list[str]):
         structure = graph_to_dict(iso, f"global_{i}")
         summary["GLOBAL"].append(structure)
         save_to_file(structure, f"global_{i}.json", folder_name=export_folder)
-        graph_to_pdf(iso, f"global_{i}.json", export_folder)
+        graph_to_pdf(iso, f"global_{i}", export_folder)
 
     save_to_file(summary, f"summary.json", folder_name=f"analysis/isomorphisms/{key_nodes_str}")
+    return summary
+
+
+def predefined_nodes(designs_chosen):
+    key_nodes = ["BatteryController", "Tube", "Hub2", "Hub3", "Hub4", "Hub5", "Hub6"]
+    return explore_structures(designs_chosen, key_nodes)
+
+
+def random_sampling(designs_chosen, nodes_types):
+    subset = random.sample(nodes_types, random.randint(1, len(nodes_types)))
+    print(f"Node keys: {subset}")
+    return explore_structures(designs_chosen, subset), subset
+
+
+def random_sampling_for_n(iterations: int = 10000):
+    designs_chosen = [d[0] for d in designs.values()]
+    nodes_types = set()
+    for d in designs_chosen:
+        nodes_types |= d.all_comp_types_ids
+
+    iteration = 0
+    total_summary = {}
+
+    while(iteration < iterations):
+        print(f"Iteration: {iteration}")
+        summary, subset = random_sampling(designs_chosen, nodes_types)
+        summary_file = output_folder / "analysis/isomorphisms/structure_summary.json"
+        exiting_entries = 0
+        if summary_file.is_file():
+            existing_summary: dict = json.load(open(output_folder / "analysis/isomorphisms/structure_summary.json"))
+            exiting_entries = len(existing_summary.keys())
+            total_summary = existing_summary
+        l_hits = len(summary["LOCAL"])
+        g_hits = len(summary["GLOBAL"])
+        t_hits = l_hits + g_hits
+        total_summary[str(exiting_entries + iteration)] = {"HITS": t_hits, "LOCAL": l_hits, "GLOBAL": g_hits, "KEYS": list(subset)}
+
+        save_to_file(total_summary, "structure_summary.json", f"analysis/isomorphisms/")
+        iteration += 1
+
 
 
 if __name__ == "__main__":
-    dconcretes = [d[0] for d in designs.values()]
-    key_nodes = ["BatteryController", "Tube", "Hub2", "Hub3", "Hub4", "Hub5", "Hub6"]
-    explore_structures(dconcretes, key_nodes)
+    designs_chosen = [d[0] for d in designs.values()]
+    # predefined_nodes(designs_chosen)
+    random_sampling_for_n(4)
