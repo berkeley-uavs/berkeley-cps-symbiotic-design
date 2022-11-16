5980742e18132127ec915a19d555b3da2163af9c
pier <piergiuseppe.mallozzi@gmail.com>
2022-11-16 14:40:08 -0800

    updated tests

--- tests/test_abastract_topology.py
+++ tests/test_abastract_topology.py
@@ -6,33 +6,37 @@ from sym_cps.representation.design.concrete import DConcrete
 from sym_cps.shared.designs import designs
 from sym_cps.shared.objects import ExportType
 
+designs_to_check = ["NewAxe_Cargo", "PickAxe", "TestQuad_Cargo"]
+
 
 def assert_topology_from_and_to_json(topology_level: ExportType):
-    """Test of AbstractTopology at the level of abstraction 1 (lowest)"""
-    # Loading DConcrete Object
-    test_quad_original = designs["TestQuad"][0]
+    for design_id in designs_to_check:
+        assert_topology_from_and_to_json_design(designs[design_id][0], topology_level)
+
+
+def assert_topology_from_and_to_json_design(design_original: DConcrete, topology_level: ExportType):
 
     # Exporting AbstractTopology to file
-    topology_json_path_1 = test_quad_original.export(topology_level)
+    topology_json_path_1 = design_original.export(topology_level)
 
     # Loading AbstractTopology from file
     abstract_topology = AbstractTopology.from_json(topology_json_path_1)
 
     # Creating DConcrete Object
-    test_quad_loaded = DConcrete.from_abstract_topology(abstract_topology)
+    design_loaded = DConcrete.from_abstract_topology(abstract_topology)
 
-    assert test_quad_original == test_quad_loaded
+    assert design_original == design_loaded
 
     # Exporting AbstractTopology to file
-    topology_json_path_2 = test_quad_loaded.export(topology_level)
+    topology_json_path_2 = design_loaded.export(topology_level)
 
     # Loading again from file
     abstract_topology_2 = AbstractTopology.from_json(topology_json_path_2)
 
     # Creating DConcrete Object
-    test_quad_loaded_2 = DConcrete.from_abstract_topology(abstract_topology_2)
+    design_loaded_2 = DConcrete.from_abstract_topology(abstract_topology_2)
 
-    assert test_quad_original == test_quad_loaded_2
+    assert design_original == design_loaded_2
 
 
 levels = [ExportType.TOPOLOGY_1, ExportType.TOPOLOGY_2, ExportType.TOPOLOGY_3, ExportType.TOPOLOGY_4]
@@ -52,7 +56,6 @@ def test_topology_abstraction_3():
 
 def test_topology_abstraction_4():
     assert_topology_from_and_to_json(ExportType.TOPOLOGY_4)
-
 
 
 test_topology_abstraction_1()
