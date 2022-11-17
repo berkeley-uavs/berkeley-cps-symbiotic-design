d0617e1870259237aab9d5d4ab08c4bcef835608
pier <piergiuseppe.mallozzi@gmail.com>
2022-11-16 16:05:58 -0800

    design generator

---
+++ src/sym_cps/grammar/generator.py
@@ -0,0 +1,8 @@
+class Grid:
+    nodes: list[list[list[str]]]
+    adjacencies: dict[tuple, list[tuple]]
+
+
+def generate_design(grammar_rules: dict | None = None,
+                    size_grid: int | None = None) -> Grid:
+    pass
