ab2b8d486722148dedfa5c22d58bd66f8723f6ef
pier <piergiuseppe.mallozzi@gmail.com>
2022-12-01 12:12:57 -0800

    processing

---
+++ src/sym_cps/grammar/contracts.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from sym_cps.shared.paths import data_folder
+
+rule_dict_path_constant = data_folder / "reverse_engineering" / "grammar_rules_new.json"
+
+
+@dataclass
+class Contract:
+    assumptions: list[str]
+    guarantees: dict[tuple, list[tuple]]
+    name: str = ""
+
+
+
