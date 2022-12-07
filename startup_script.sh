83d04897b29ad3542571c9ec5e7816d0c9eb6db9
pier <piergiuseppe.mallozzi@gmail.com>
2022-12-06 23:54:43 -0800

    small fix

--- startup_script.sh
+++ startup_script.sh
@@ -3,20 +3,23 @@ git pull
 
 if [ $# -eq 0 ]
   then
-    echo "No parameters provided, background mode"
+    echo "No parameters provided. Launching bash"
+    bash
 else
-    case "$1" in
-      -h|--help)
-        echo "options:"
-        echo "-h, --help                show brief help"
-        exit 0
-        ;;
-      bash)
-        echo "Launching bash..."
-        bash
-        exit 0
-        ;;
-      *)
-        ;;
-    esac
+    while test $# -gt 0; do
+      case "$1" in
+        -h|--help)
+          echo "options:"
+          echo "-h, --help                show brief help"
+          echo "-r,                       launch random designs evaluation"
+          exit 0
+          ;;
+        -r)
+          bash
+          ;;
+        *)
+          break
+          ;;
+      esac
+    done
 fi
