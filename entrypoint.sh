f1321854f364308ddaee0355b9d7dc39d8b919cc
pier <piergiuseppe.mallozzi@gmail.com>
2022-12-07 23:16:14 -0800

    updated docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,5 @@ git config --global --add safe.directory /root/host
 echo "Pulling from github..."
 git pull
 
-echo "Run docker-init"
-make docker-init
-
 echo "Starting startup script..."
 source startup_script.sh "$@"
