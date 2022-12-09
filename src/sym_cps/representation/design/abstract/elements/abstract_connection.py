59ccb0c607ad9077619fbc23f856bbd028ed3cbe
brendanh189 <brendanh189@berkeley.edu>
2022-12-08 19:47:38 -0800

    save

--- src/sym_cps/representation/design/abstract/elements/abstract_connection.py
+++ src/sym_cps/representation/design/abstract/elements/abstract_connection.py
@@ -37,14 +37,14 @@ class AbstractConnection:
                 Direction.left: "Hub4__Side_Connector_4",
                 Direction.right: "Hub4__Side_Connector_2",
                 Direction.top: "Hub4__Top_Connector",
-                Direction.bottom: "Hub4__Center_Connector",
+                Direction.bottom: "Hub4__Bottom_Connector",
             },
             "Flange": {
                 Direction.front: "Flange__SideConnector",
                 Direction.rear: "Flange__SideConnector",
                 Direction.left: "Flange__SideConnector",
                 Direction.right: "Flange__SideConnector",
-                Direction.top: "Flange__BottomConnector",  # Flange__TopConnector ?
+                Direction.top: "Flange__BottomConnector",
                 Direction.bottom: "Flange__BottomConnector",
             },
         }
@@ -70,12 +70,12 @@ class AbstractConnection:
         top_tube_connector = c_library.connectors["Tube__EndConnection"]
 
         if component_a.c_type.id == "Hub4":
-            if self.direction_from_a_to_b == Direction.top or self.direction_from_a_to_b == Direction.bottom:
+            if self.direction_from_a_to_b == Direction.bottom:
                 bottom_tube_connector = c_library.connectors["Tube__OffsetConnection2"]
 
-        if (component_b.c_type.id == "Hub4" and
-                (self.direction_from_b_to_a == Direction.top or self.direction_from_b_to_a == Direction.bottom)):
-            top_tube_connector = c_library.connectors["Tube__OffsetConnection1"]
+        if component_b.c_type.id == "Hub4":
+            if self.direction_from_b_to_a == Direction.bottom:
+                top_tube_connector = c_library.connectors["Tube__OffsetConnection1"]
 
 
         connection_a_tube = Connection(
